8071de434170f099952e44dccffa5c8a9dc5922b
nioomeee <nioomeee@gmail.com>
2025-10-19 23:51:42 +0530

    use BufferedReader and BufferedWriter to read and write from and to a file

---
+++ I/I-O/Cloner.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Cloner {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+                String currentLine;
+
+                while((currentLine = reader.readLine()) != null) {
+                    writer.write(currentLine);
+                    writer.newLine();
+                }
+        } catch (IOException e) {
+            System.err.println("An error occured: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        System.out.println("output printed successfully");
+    }
+
+}
