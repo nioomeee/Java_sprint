b0dd69423b7fe490879000a0516b12cd2511f73f
nioomeee <nioomeee@gmail.com>
2025-10-24 17:13:33 +0530

    attempt 3

--- Calculator/complexCalc.java
+++ Calculator/complexCalc.java
@@ -15,87 +15,90 @@ class FatalError extends Exception {
 }
 
 public class complexCalc {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         Pattern declare = Pattern.compile("([a-zA-Z]+)\\s*=\\s*([+-]?\\d+)\\s*;?");
-        Pattern assign = Pattern.compile("([a-zA-Z]+)\\s*([+\\-*/]?=)\\s*([+-]?\\d+|[a-zA-Z]+)\\s*;");
 
-        Map<String, Integer> Values = new HashMap<>();
-        Map<String, Integer> Lines = new HashMap<>();
+        Pattern assign = Pattern.compile("([a-zA-Z]+)\\s*([+\\-*/]?=)\\s*([+-]?\\d+|[a-zA-Z]+)\\s*;?");
 
-        int lineNum = 0;
+        Map<String, Integer> values = new HashMap<>();
+        Map<String, Integer> lines = new HashMap<>();
+
+        int line = 0;
+
         String fatalMsg = null;
 
         try(BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
-
             String currentLine;
 
             while((currentLine = reader.readLine()) != null) {
-                lineNum++;
+                line++;
 
                 try {
+
                     if(currentLine.trim().startsWith("int ")) {
-                        Matcher m = declare.matcher(currentLine); 
+                        Matcher m = declare.matcher(currentLine);
 
                         while(m.find()) {
                             String name = m.group(1);
                             int value = Integer.parseInt(m.group(2));
 
-                            Values.put(name, value);
-                            Lines.put(name, lineNum);
+                            values.put(name, value);
+                            lines.put(name, line);
                         }
                     } else {
                         Matcher m = assign.matcher(currentLine);
-
                         if(m.matches()) {
                             String name = m.group(1);
                             String operator = m.group(2);
                             String valString = m.group(3);
 
                             int value;
-                            
+
                             try {
                                 value = Integer.parseInt(valString);
-                            } catch(NumberFormatException e) {
-                                if(!Values.containsKey(valString)) {
-                                    throw new FatalError("Uninitialized Variable");
+                            } catch (NumberFormatException e) {
+                                if(!values.containsKey(valString)) {
+                                    throw new FatalError("Uninitialized variable");
                                 }
-                                value = Values.get(valString);
+                                value = values.get(valString);
                             }
 
                             switch(operator) {
                                 case "=":
-                                    Values.put(name, value);
+                                    values.put(name, value);
                                     break;
 
                                 case "+=":
-                                case "-=":
-                                case "*=":
-                                case "/=":
-                                    if(!Values.containsKey(name)) {
-                                        throw new FatalError("Uninitialized variable");
-                                    }
 
-                                    int oldVal = Values.get(name);
+                                case "-=":
+
+                                case "*=":
+
+                                case "/=":
+                                    if(!values.containsKey(name)) {
+                                        throw new FatalError("Uninnitialized Variable");
+                                    }
+                                    int oldVal = values.get(name);
 
                                     if(operator.equals("+=")) {
-                                        Values.put(name, oldVal + value);
+                                        values.put(name, oldVal + value);
                                     } 
-                                    if(operator.equals("-=")) {
-                                        Values.put(name, oldVal - value);
+                                    if (operator.equals("-=")) {
+                                        values.put(name, oldVal - value);
                                     }
                                     if(operator.equals("*=")) {
-                                        Values.put(name, oldVal * value);
+                                        values.put(name, oldVal * value);
                                     }
                                     if(operator.equals("/=")) {
                                         if(value == 0) {
-                                            throw new FatalError("Error: Division by 0");
+                                            throw new FatalError("division by 0");
+                                        } else {
+                                            values.put(name, oldVal / value);
                                         }
-                                        Values.put(name, oldVal / value);
                                     }
                                     break;
                             }
-                            Lines.put(name, lineNum);
+                            lines.put(name, line);
                         }
                     }
 
@@ -103,11 +106,9 @@ public class complexCalc {
                     fatalMsg = e.getMessage();
                     break;
                 }
-
             }
-
         } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
+            System.err.println("Error reading file: " + e.getMessage());
             e.printStackTrace();
         }
 
@@ -115,32 +116,30 @@ public class complexCalc {
             if(fatalMsg != null) {
                 writer.write(fatalMsg);
             } else {
-                String winner = "";
                 int maxVal = Integer.MIN_VALUE;
-                int maxLine =  -1;
-
-
-                for(Map.Entry<String, Integer> entry : Values.entrySet()) {
+                int maxLine = -1;
+                String winner = "";
+                for(Map.Entry<String, Integer> entry : values.entrySet()) {
                     String name = entry.getKey();
                     int value = entry.getValue();
-                    int line = Lines.get(name);
+                    int currLine  = lines.get(name);
 
-                    if(value > maxVal) {
-                        maxVal = value;
+                    if (value > maxVal) {
                         winner = name;
-                        maxLine = line;
+                        maxVal = value;
+                        maxLine = currLine;
                     } else if(value == maxVal) {
-                        if(line > maxLine) {
-                            maxLine = line;
+                        if(currLine > maxLine) {
+                            maxLine = currLine;
                             winner = name;
                         }
                     }
                 }
-
                 writer.write(String.valueOf(maxVal));
             }
-        } catch (IOException e) {
-            
+        } catch(IOException e) {
+            System.err.println("Error writing to the file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
